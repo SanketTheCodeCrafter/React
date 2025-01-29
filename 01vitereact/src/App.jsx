052caf04ed3fbe15d153fd7bb214c110917ff75a
SanketTheCodeCrafter <sanket.nagap3@gmail.com>
2025-01-29 21:08:02 +0530

    Today, I learned how to create custom React components and how the React tree works—specifically, how HTML and JavaScript are parsed. I also explored evaluated expressions that can be passed when creating React elements.

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -4,9 +4,11 @@ import Example from "./example"
 function App() {
 
 
+  const comment="I am Sanket!"
+
   return (
     <>   {/* This is a fragment element used to wrap multiple elements at once as a single element because the component can only return only single element. */}
-    <h1>Hello World, This is React with Vite!</h1>
+    <h1>Hello World, This is React with Vite! {comment} </h1>
     <Example />
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,49 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
+// <---- This will not render anything because it is incorrect method
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'http://www.google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit Google' // fixed case
+// }
+
+
+const username="Sanket"
+
+// <---- This can be used to render the react component
+const reactElement=React.createElement(
+  'a',
+  {
+    href:'http://www.google.com',
+    target: '_blank'
+  },
+  'Click to visit Google. This is rendered by reactElement',
+  username
+)
+
+// <---- This is too another way to render the react component
+const anotherElement=(
+  <a href="http://www.google.com" target="_blank">Click me to visit Google. This is rendered by anotherElement</a>
+)
+
+
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+
+  
+  // reactElement
+
+  anotherElement
+
+
 )
