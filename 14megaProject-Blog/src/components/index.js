8b1076e30cc7bf628bafa70e94020f23531f81e6
SanketTheCodeCrafter <sanket.nagap3@gmail.com>
2025-02-25 21:44:09 +0530

    feat: integrate TinyMCE RTE in PostForm

--- 14megaProject-Blog/src/components/index.js
+++ 14megaProject-Blog/src/components/index.js
@@ -3,11 +3,13 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
+import RTE from "./RTE";
 
 export {
     Header,
     Container,
     LogoutBtn,
     Logo,
-    Footer
+    Footer,
+    RTE
 }
